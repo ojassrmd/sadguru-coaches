8c89ace3c17f7f57677072cd9d3e167cd85a589d
ojasp <ojas.pipalia@gmail.com>
2018-03-01 00:20:38 +0530

    added more logs

--- core/components/login/controllers/init.js
+++ core/components/login/controllers/init.js
@@ -69,9 +69,9 @@ angular.module('mm.core.login')
             }
         } else {
             $mmSitesManager.hasSites().then(function() {
-                return $state.go('mm_login.sites');
+                //return $state.go('mm_login.sites');
                 $log.debug("loadCurrent");
-                //$state.go('mm_login.credentials', {siteurl: mmCoreConfigConstants.siteurl});
+                return $state.go('mm_login.credentials', {siteurl: mmCoreConfigConstants.siteurl});
             }, function() {
                 return $mmLoginHelper.goToAddSite();
             });
